ec50b74e0c6de07ec5a70b53737bb6bae513a3d6
LCS <992874270@qq.com>
2017-03-12 22:13:46 +0800

    MySQL 分表备份

---
+++ MySQL/mysql_database_back.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#coding:utf-8
+#mysql分库备份
+PWD="/data/backup/"
+EXPIRE=30
+mkdir -p /data/backup/$(date +%F)
+USER=root
+PASS=123456
+LOGIN="mysql -u$USER -p$PASS"
+MYDUMP="mysqldump -u$USER -p$PASS"
+for database in `$LOGIN -e "show databases;" | egrep -v "mysql|*schema|Database|*found|backup"`
+do
+    #$MYDUMP $database | gzip >/data/backup/$(date +%F)/${database}_$(date +%F).bak.gz
+    $MYDUMP $database | gzip >/data/backup/$(date +%F)/${database}.bak.gz
+done
+find $PWD -type f -mtime +${EXPIRED} -exec rm -fr {}
