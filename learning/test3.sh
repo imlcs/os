41fed4400f5d69719aae612ac6118b085d1fa8c5
lcs <lcs@salt.xdl.com>
2017-03-16 22:15:21 +0800

    add note

--- learning/test3.sh
+++ learning/test3.sh
@@ -6,15 +6,18 @@ function show_usage()
 	exit 1
 }
 
+#判定参数个数
 if [ $# -ne 2 ];then
 	show_usage
 else
+	#判定第一个参数的类型
 	if [ -d $1 ];then 
 		source_dir=$1
 	else
 		echo "Invalid source directory"
 		show_usage
 	fi
+	#判定第二个参数的类型
 	if [ -d $2 ];then
 		dest_dir=$2
 	else
