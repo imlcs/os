d9dc7719ea59a455c120d08f549ac5bb5c972996
LCS <992874270@qq.com>
2017-07-22 16:46:31 +0800

    Tomcat环境变量配置，放入/etc/profile.d/目录下

---
+++ Tomcat/java.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: /etc/profile.d/java.sh
+# Author: lcs
+# mail: liuchengsheng95@qq.com
+# Created Time: 2017-07-15 17:33:44
+#########################################################################
+
+#!/bin/bash
+export JAVA_HOME=/usr/java/latest
+export PATH=$JAVA_HOME:$PATH
---
+++ Tomcat/tomcat.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: /etc/profile.d/tomcat.sh
+# Author: lcs
+# mail: liuchengsheng95@qq.com
+# Created Time: 2017-07-15 18:11:24
+#########################################################################
+
+#!/bin/bash
+export CATALINA_HOME=/usr/local/tomcat
+export PATH=$CATALINA_HOME/bin:$PATH
